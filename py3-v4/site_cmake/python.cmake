f701ead0b4b5671a88eb9cc3c474085418556acf
Sasha Rahlin <arahlin@fnal.gov>
2024-03-18 00:19:45 -0600

    use correct python version in py3-v4/site_cmake
    is this file even necessary anymore?

--- py3-v4/site_cmake/python.cmake
+++ py3-v4/site_cmake/python.cmake
@@ -1,10 +1,10 @@
-set(PYTHON_LIBRARIES "$ENV{SROOT}/lib/libpython3.6.so" CACHE FILEPATH "Python library")
-set(PYTHON_INCLUDE_DIR "$ENV{SROOT}/include/python3.6" CACHE PATH "Path the Python include directory")
+set(PYTHON_LIBRARIES "$ENV{SROOT}/lib/libpython3.10.so" CACHE FILEPATH "Python library")
+set(PYTHON_INCLUDE_DIR "$ENV{SROOT}/include/python3.10" CACHE PATH "Path the Python include directory")
 set(PYTHON_EXECUTABLE "$ENV{SROOT}/bin/python" CACHE FILEPATH "Python interpreter")
-set(PYTHON_VERSION "3.6" CACHE STRING "Python version")
+set(PYTHON_VERSION "3.10" CACHE STRING "Python version")
 set(PYTHON_FOUND TRUE CACHE BOOL "Python found")
 
-set(PYTHON_NUMERIC_VERSION 30601)
+set(PYTHON_NUMERIC_VERSION 31001)
 
 set(NUMPY_FOUND TRUE CACHE BOOL "Numpy found successfully" FORCE)
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
