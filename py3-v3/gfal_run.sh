484d6e216c47a8a7dd5111b426849f534a9f55ae
Sasha Rahlin <arahlin@uchicago.edu>
2024-11-08 15:53:33 -0600

    Use more recent version of oasis if possible

--- py3-v3/gfal_run.sh
+++ py3-v3/gfal_run.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 case $OS_ARCH in
+    RHEL_9_x86_64)
+        OSG_ARCH=el9-x86_64
+        OSG_VERSION=23-main
+        ;;
     RHEL_8_x86_64)
         OSG_ARCH=el8-x86_64
+        OSG_VERSION=23-main
         ;;
     RHEL_7_x86_64)
         OSG_ARCH=el7-x86_64
+        OSG_VERSION=3.6
         ;;
     RHEL_6_x86_64)
         OSG_ARCH=el6-x86_64
+        OSG_VERSION=3.4
         ;;
     *)
         echo "Unknown architecture"
@@ -27,6 +34,6 @@ for pth in `echo $PATH|tr ":" "\n"`; do
 done
 export PATH=$NEWPATH
 unset NEWPATH PYTHONPATH LD_LIBRARY_PATH PERL5LIB
-source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/current/$OSG_ARCH/setup.sh
+source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/$OSG_VERSION/current/$OSG_ARCH/setup.sh
 
 exec $func $args
--- py3-v4/gfal_run.sh
+++ py3-v4/gfal_run.sh
@@ -3,15 +3,19 @@
 case $OS_ARCH in
     RHEL_9_x86_64)
         OSG_ARCH=el9-x86_64
+        OSG_VERSION=23-main
         ;;
     RHEL_8_x86_64)
         OSG_ARCH=el8-x86_64
+        OSG_VERSION=23-main
         ;;
     RHEL_7_x86_64)
         OSG_ARCH=el7-x86_64
+        OSG_VERSION=3.6
         ;;
     RHEL_6_x86_64)
         OSG_ARCH=el6-x86_64
+        OSG_VERSION=3.4
         ;;
     *)
         echo "Unknown architecture"
@@ -30,6 +34,6 @@ for pth in `echo $PATH|tr ":" "\n"`; do
 done
 export PATH=$NEWPATH
 unset NEWPATH PYTHONPATH LD_LIBRARY_PATH PERL5LIB
-source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/3.6/current/$OSG_ARCH/setup.sh
+source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/$OSG_VERSION/current/$OSG_ARCH/setup.sh
 
 exec $func $args
