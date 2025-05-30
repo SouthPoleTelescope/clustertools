5abd1761725bdc95fc4972805ac4e69b0b8f1675
Sasha Rahlin <arahlin@uchicago.edu>
2025-05-30 14:10:56 -0500

    Rename again, for consistency with X509 UI

--- py3-v3/gfal_run.sh
+++ py3-v3/gfal_run.sh
@@ -32,20 +32,20 @@ export PATH=$NEWPATH
 unset NEWPATH PYTHONPATH LD_LIBRARY_PATH PERL5LIB
 source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/$OSG_VERSION/current/$OSG_ARCH/setup.sh
 
-if [ -z "$GRID_TOKEN" ]; then
+if [ -z "$GRID_USER_TOKEN" ]; then
     TOKEN_FILE="/tmp/token_u$(/usr/bin/id -u)"
     if [ -e $TOKEN_FILE ]; then
-	GRID_TOKEN=$TOKEN_FILE
+	GRID_USER_TOKEN=$TOKEN_FILE
     fi
 fi
 
-if [ -n "$GRID_TOKEN" ]; then
-    if [[ $(find "$GRID_TOKEN" -mtime 72 -print) ]]; then
-	echo "Grid token $GRID_TOKEN is out of date, please generate a new one using token-init"
+if [ -n "$GRID_USER_TOKEN" ]; then
+    if [[ $(find "$GRID_USER_TOKEN" -mtime 72 -print) ]]; then
+	echo "Grid token $GRID_USER_TOKEN is out of date, please generate a new one using token-init"
 	exit 1
     fi
     unset X509_USER_PROXY
-    export BEARER_TOKEN=`cat $GRID_TOKEN`
+    export BEARER_TOKEN=`cat $GRID_USER_TOKEN`
 fi
 
 exec $func $args
