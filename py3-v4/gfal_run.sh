7109fe5d41d9812f30be99d239f8a479fbf2b526
Sasha Rahlin <arahlin@uchicago.edu>
2025-06-13 14:21:45 -0500

    Keep using voms proxy for production user for now

--- py3-v4/gfal_run.sh
+++ py3-v4/gfal_run.sh
@@ -32,24 +32,27 @@ export PATH=$NEWPATH
 unset NEWPATH PYTHONPATH LD_LIBRARY_PATH PERL5LIB
 source /cvmfs/oasis.opensciencegrid.org/osg-software/osg-wn-client/$OSG_VERSION/current/$OSG_ARCH/setup.sh
 
-if [ -z "$GRID_USER_TOKEN" ]; then
-    TOKEN_FILE="/tmp/token_u$(/usr/bin/id -u)"
-    if [ -e $TOKEN_FILE ]; then
-	GRID_USER_TOKEN=$TOKEN_FILE
+if [ "$USER" != "spt" ]; then
+    if [ -z "$GRID_USER_TOKEN" ]; then
+	TOKEN_FILE="/tmp/token_u$(/usr/bin/id -u)"
+	if [ -e $TOKEN_FILE ]; then
+	    GRID_USER_TOKEN=$TOKEN_FILE
+	fi
     fi
-fi
 
-if [ -n "$GRID_USER_TOKEN" ]; then
+    if [ -z "$GRID_USER_TOKEN" ]; then
+	echo "Missing grid token, please use token-init to generate one"
+	exit 1
+    fi
+
     if [[ $(find "$GRID_USER_TOKEN" -mtime 72 -print) ]]; then
 	echo "Grid token $GRID_USER_TOKEN is out of date, please generate a new one using token-init"
 	exit 1
     fi
+
     voms-proxy-destroy &> /dev/null
     unset X509_USER_PROXY
     export BEARER_TOKEN=`cat $GRID_USER_TOKEN`
-else
-    echo "Missing grid token, please use token-init to generate one"
-    exit 1
 fi
 
 exec $func $args
